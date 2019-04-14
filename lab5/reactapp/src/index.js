090bf2c431383fb3c804e613637df1860a6bf459
towqsz <tomasz.garski@gmail.com>
2019-04-14 09:37:21 +0200

    After lab 5

---
+++ lab5/reactapp/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+
+function Message() {
+    return (
+    <h2> Helloe! </h2>
+    )
+}
+
+
+ReactDOM.render(<Message />, document.getElementById('root'));
